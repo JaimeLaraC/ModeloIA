2eaae2796dd0963afc4503b503a9f722d6316778
JaimeLaraC <jailacont@gmail.com>
2024-11-05 12:48:37 +0100

    Actualizacion Entorno

--- src/EntornoVirtual.py
+++ src/EntornoVirtual.py
@@ -13,7 +13,7 @@ def create_venv_and_install_packages():
         activate_script = ".venv/bin/activate"
 
     # Ejecutar los comandos en el entorno virtual con la opción --break-system-packages
-    command = f"source {activate_script} && pip install tensorflow numpy pandas --break-system-packages"
+    command = f"source {activate_script} && pip install tensorflow numpy pandas tensorflow_datasets --break-system-packages"
     subprocess.call(command, shell=True, executable="/bin/bash")
 
     print("Entorno virtual '.venv' creado y paquetes instalados: tensorflow, numpy, pandas.")
